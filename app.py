8ff661e490cf3e7f72604617cbf555fe03ca5bc6
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-30 19:46:21 -0400

    Index route, seed products

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+# @app.route("/photos.json")
+# def photos_index():
+#     return db.photos_all()
+
+@app.route("/products.json")
+def products_index():
+    return db.products_all()
--- db.py
+++ db.py
@@ -7,37 +7,75 @@ def connect_to_db():
     return conn
 
 
-def initial_setup():
+# def initial_setup():
+#     conn = connect_to_db()
+#     conn.execute(
+#         """
+#         DROP TABLE IF EXISTS photos;
+#         """
+#     )
+#     conn.execute(
+#         """
+#         CREATE TABLE photos (
+#           id INTEGER PRIMARY KEY NOT NULL,
+#           name TEXT,
+#           width INTEGER,
+#           height INTEGER
+#         );
+#         """
+#     )
+#     conn.commit()
+#     print("Table created successfully")
+
+#     photos_seed_data = [
+#         ("1st photo", 800, 400),
+#         ("2nd photo", 1024, 768),
+#         ("3rd photo", 200, 150),
+#     ]
+#     conn.executemany(
+#         """
+#         INSERT INTO photos (name, width, height)
+#         VALUES (?,?,?)
+#         """,
+#         photos_seed_data,
+#     )
+#     conn.commit()
+#     print("Seed data created successfully")
+
+#     conn.close()
+
+
+def products_setup():
     conn = connect_to_db()
     conn.execute(
         """
-        DROP TABLE IF EXISTS photos;
+        DROP TABLE IF EXISTS products;
         """
     )
     conn.execute(
         """
-        CREATE TABLE photos (
+        CREATE TABLE products (
           id INTEGER PRIMARY KEY NOT NULL,
           name TEXT,
-          width INTEGER,
-          height INTEGER
+          category TEXT,
+          price DECIMAL
         );
         """
     )
     conn.commit()
     print("Table created successfully")
 
-    photos_seed_data = [
-        ("1st photo", 800, 400),
-        ("2nd photo", 1024, 768),
-        ("3rd photo", 200, 150),
+    products_seed_data = [
+        ("Lagavulin", "Scotch", 80),
+        ("Pappy", "Bourbon", 5499),
+        ("Tito's", "Vodka", 20),
     ]
     conn.executemany(
         """
-        INSERT INTO photos (name, width, height)
+        INSERT INTO products (name, category, price)
         VALUES (?,?,?)
         """,
-        photos_seed_data,
+        products_seed_data,
     )
     conn.commit()
     print("Seed data created successfully")
@@ -46,4 +84,23 @@ def initial_setup():
 
 
 if __name__ == "__main__":
-    initial_setup()
+    products_setup()
+
+
+def photos_all():
+    conn = connect_to_db()
+    rows = conn.execute(
+        """
+        SELECT * FROM photos
+        """
+    ).fetchall()
+    return [dict(row) for row in rows]
+
+def products_all():
+    conn = connect_to_db()
+    rows = conn.execute(
+        """
+        SELECT * FROM products
+        """
+    ).fetchall()
+    return [dict(row) for row in rows]
